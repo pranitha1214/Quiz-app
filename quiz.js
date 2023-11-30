86f6f641f4892aeb286d8a59424a71a8ef712334
pranitha1214 <140951706+pranitha1214@users.noreply.github.com>
2023-11-30 20:31:27 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,77 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of these directories contain device files?",
+    answer: "/dev",
+    options: [
+      "/dev",
+      "/home",
+      "/lib",
+      "/media"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of these is the first step of penetration testing?",
+    answer: "Information gathering",
+    options: [
+      "Exploitation",
+      "Scanning",
+      "Information gathering",
+      "Getting/maintaing access"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does DHCP stand for?",
+    answer: "dynamic host configuration protocol",
+    options: [
+      "domain host configuration protocol",
+      "Digital Host Control Protocol",
+      "Data Handling and Configuration Protocol",
+      "dynamic host configuration protocol"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of these is unique?",
+    answer: "MAC address",
+    options: [
+      "IP address",
+      "Newtwork mask",
+      "MAC address",
+      "Network address translation"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How many steps of included in penetration testing?",
+    answer: "five",
+    options: [
+      "five",
+      "seven",
+      "nine",
+      "eleven"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
+
+
+
+
+
+
